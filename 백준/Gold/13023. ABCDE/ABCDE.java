9e3848a999f7eeb0b0af3ef9b947db1588286ef3
ssDoi <108009177+ssDoi@users.noreply.github.com>
2024-02-21 20:49:49 +0900

    [Gold V] Title: ABCDE, Time: 256 ms, Memory: 18896 KB -BaekjoonHub

--- 백준/Gold/13023. ABCDE/ABCDE.java
+++ 백준/Gold/13023. ABCDE/ABCDE.java
@@ -2,10 +2,10 @@ import java.io.*;
 import java.util.*;
 
 /**
- * 문제: Main_15683_감시
+ * 문제: Main_13023_ABCDE
  * 결과: 맞았습니다!
- * 메모리:69864kb
- * 실행시간: 352ms
+ * 메모리:18904kb
+ * 실행시간: 264ms
  * @author 서동인
  * 아이디어: 카메라의 종류와 인덱스를 리스트에 저장한다.
  * 중복 순열을 사용하여 각 카메라의 방향을 설정한다.
@@ -17,7 +17,7 @@ public class Main {
 	static boolean[] visited;
 	//4연쇄 친구가 맞는지 확인
 	static boolean valid;
-	//그래프
+	//그래프 인접 리스트
 	static ArrayList<ArrayList<Integer>> graph;
 	public static void main(String[] args) throws Exception{
 		//입력을 위한 버퍼드리더, 스트링토크나이저
@@ -50,26 +50,25 @@ public class Main {
 			visited = new boolean[n];
 			dfs(i,0);
 		}
-		//친구관계가 이뤄지면 1 아니면 0
-		if(valid) {
-			System.out.println(1);
-		}else {
+		//친구관계가 안이뤄지면 0 출력
+		if(!valid) {
 			System.out.println(0);
 		}
 	}
 	static void dfs(int current, int cnt) {
-		if(cnt==4) {
+		if(cnt==4) {	//친구관계완성 되면 1출력하고 시스템종료
 			valid=true;
 			System.out.println(1);
 			System.exit(0);
-			return;
 		}
 		visited[current]= true;
 		// 탐색정점에 관련된 로직처리
 		//그래프 확인
 		for (int i = 0; i < graph.get(current).size(); i++) {
+			//그래프에 있는 리스트의 정점이 방문안된 거면 dfs
 			if(!visited[graph.get(current).get(i)]) {
 					dfs(graph.get(current).get(i),cnt+1);
+					//다시 돌아와 다음 정점 false하고 다음 순회
 					visited[graph.get(current).get(i)] = false;
 				}
 		}
