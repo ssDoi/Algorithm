b13ffabed58062651c582c83f669a421e553502a
ssDoi <108009177+ssDoi@users.noreply.github.com>
2024-04-01 17:31:51 +0900

    [D3] Title: [Professional] 조합, Time: 441 ms, Memory: 86,384 KB -BaekjoonHub

--- SWEA/D3/5607. ［Professional］ 조합/［Professional］ 조합.java
+++ SWEA/D3/5607. ［Professional］ 조합/［Professional］ 조합.java
@@ -1,6 +1,14 @@
 import java.io.*;
 import java.util.*;
-
+/**
+ * 문제: SWEA_8458_원점으로집합
+ * 결과: 맞았습니다 
+ * 메모리:37,928kb 
+ * 실행시간: 324ms
+ * @author 서동인 
+ * 아이디어: 모든 좌표의 맨해튼 거리가 모두 짝수이거나 홀수여야 조건을 만족한다. 제일 먼 좌표의 거리보다 1~i를 모두 더한 값이 더 크고 좌표의 거리가
+ * 짝수 일 경우 짝수, 홀수일 경우 홀수여야한다.
+ */
 public class Solution {
 
 	public static void main(String[] args) throws Exception{
@@ -20,9 +28,8 @@ public class Solution {
 
 
 	}
-
     // 페르마의 소정리를 사용하여 역원을 구하는 메서드
-    public static long modularInverse(long a, int p) {
+    public static long Inverse(long a, int p) {
         long result = 1;
         while (a > 1) {
             result = (result * (p - p / a)) % p;
@@ -38,8 +45,8 @@ public class Solution {
         for(int i=2;i<n+1;i++) {
         	fac[i] = fac[i-1] *i %p;
         }
-        long inverseDenominator = modularInverse((fac[k] * fac[n-k])%p, p);
-        return (fac[n] * inverseDenominator) % p;
+        long inverse = Inverse((fac[k] * fac[n-k])%p, p);
+        return (fac[n] * inverse) % p;
     }
 
 }
